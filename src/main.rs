182c24f911a4c7660c994d2eb02a5ce67579c193
Stefano Chiodino <LouderTH@gmail.com>
2017-02-22 00:17:33 +0000

    basic implementation

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    fizzbuzz_to(10000000);
+}
+
+fn fizzbuzz_to(n: u32) {
+    for n in 1..n + 1 {
+        print!("{:?} : ", n);
+
+        match n {
+            _ if is_divisible_by(n, 15) => println!("Fizzbuzz"),
+            _ if is_divisible_by(n, 3) => println!("fizz"),
+            _ if is_divisible_by(n, 5) => println!("buzz"),
+            _ => println!(),
+        }
+    }
+}
+
+fn is_divisible_by(i: u32, j: u32) -> bool {
+    if i == 0 {
+        return false;
+    }
+
+    i % j == 0
+}
